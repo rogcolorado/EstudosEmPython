96ca1daffb95a71b9e1e90e368a04e858d5d0b52
Rogerio <rogcolorado@gmail.com>
2020-12-16 14:18:39 -0300

    at01

---
+++ teste03.py
@@ -0,0 +1,9 @@
+print('---Identificando maior e menor valor---')
+
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+if a > b:
+    print(f'{a} é maior que {b}')
+else:
+    print(f'{b} é maior que {a}')
+print('final do programa')
